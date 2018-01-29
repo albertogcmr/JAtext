8621a306d7aac97548e84f640c17f716c0dc01d1
Jesús Rodríguez Álvarez <35452093+4thSword@users.noreply.github.com>
2018-01-29 19:29:06 +0100

    solucionados los problemas de configuración
    puedes clonarte el repositorio y descargartelo otra vez. para abrir codelite abre el archivo workspace y que se clonen las opciones.

--- JAtext/JAtext/Debug/Source_JAtext_Main.c.o.d
+++
@@ -1,6 +0,0 @@
-Debug/Source_JAtext_Main.c.o: Source/JAtext_Main.c Includes/JAtext_Main.h \
- Includes/JAtext_menu.h
-
-Includes/JAtext_Main.h:
-
-Includes/JAtext_menu.h:
--- JAtext/JAtext/Debug/Source_appdata.c.o.d
+++
@@ -1,3 +0,0 @@
-Debug/Source_appdata.c.o: Source/appdata.c Includes/appdata.h
-
-Includes/appdata.h:
