9dfa5b63969446471a9f18f611d5a0c171b15ce6
Jesús Rodríguez Álvarez <35452093+4thSword@users.noreply.github.com>
2018-02-06 21:04:23 +0100

    new files implementation
    defined JAtext_operatios.c and JAtext_opertions .h

---
+++ JAtext/JAtext/Includes/JAtext_operations.h
@@ -0,0 +1,10 @@
+#ifndef _OPERATIONSH_
+#define _OPERATIOS_H_
+
+
+
+
+
+
+
+#endif
---
+++ JAtext/JAtext/Source/JAtext_operations.c
@@ -0,0 +1,11 @@
+/*
+ * File:        JAtext_operations.c
+ * Description: Contains the functios for text management functionality for the JAtext application
+ * 				Basically, it implements the different operations to modify text files.
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
